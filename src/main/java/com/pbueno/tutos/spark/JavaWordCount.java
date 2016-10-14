e4202afa45b25e5a3c90eb1b88c37f6fbf93de53
Pere <pere.bueno@gmail.com>
2016-10-14 13:32:23 +0200

    Created basic structure with showcase class

---
+++ src/main/java/com/pbueno/tutos/spark/JavaWordCount.java
@@ -0,0 +1,11 @@
+package com.pbueno.tutos.spark
+
+import java.util.regex.Pattern;
+
+public final class JavaWorldCount{
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("Testing");
+    }
+}
